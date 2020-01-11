f21d1e3d28ff21b14cb7b0ac968c28aad31e82ae
Christie Finnie <christie.finnie@gmail.com>
2020-01-11 06:37:46 -0800

    Add edit, new, and show for artists.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,45 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all.order(id: :asc)
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to(artists_path) 
+    else
+      # if nothing was defined here, the render would reuse create
+      # When using render the params is preserved. When using redirect the forms values are reset for a new session. Also whenever we change action methods
+      render(:new)
+    end
+  end
+
+  def new
+    @artist = Artist.new 
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])  
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    if @artist.update(artist_params)
+      redirect_to(artists_path)
+    else
+      render(:edit)
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to(artists_path)
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name)
+  end
+end
